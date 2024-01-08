d45dce129442b76ff7c50cae8c7e8dba1e5288bd
Bruno Alex <132787362+alexxsouzaa@users.noreply.github.com>
2024-01-08 18:13:40 -0300

    Update UltraSonic_HC_SR04.cpp
    ## Versão 1.1.0 - Lançada em 08 de janeiro de 2024
    
    ### Adições
    - Adicionada função `distancia_mm()` à classe `UltraSonic_HC_SR04` para medir a distância em milímetros.
    
    ### Alterações
    - Nenhuma alteração significativa neste lançamento.
    
    ### Correções
    - Nenhuma correção neste lançamento.
    
    ---
    
    Agradecemos por usar o nosso código!

--- src/UltraSonic_HC_SR04.cpp
+++ src/UltraSonic_HC_SR04.cpp
@@ -1,29 +1,78 @@
-#include <UltraSonic_HC_SR04.h>
-#include <Arduino.h>
+#include "UltrasonicSensor.h"
 
-UltraSonic_HC_SR04::UltraSonic_HC_SR04(int trigger, int echo)
-{
-    this->pin_trigger=trigger;
-    this->pin_echo=echo;
-    pinMode(this->pin_trigger,OUTPUT);
-    pinMode(this->pin_echo,INPUT);
+/* ------------------------------ CONSTRUCTOR CODE ------------------------------ */
 
-    digitalWrite(this->pin_trigger, LOW);
+/* Constructor - It saves the pin numbers of the Ultrasonic sensor and set them in OUTPUT and INPUT mode */
+UltrasonicSensor::UltrasonicSensor(int trigger, int echo) {
+    this->pinTrigger = trigger;
+    this->pinEcho = echo;
+    this->temperature = 20.0f;
+
+    pinMode(this->pinTrigger, OUTPUT);
+    pinMode(this->pinEcho, INPUT);
+
+    digitalWrite(this->pinTrigger, LOW);
 }
 
-int UltraSonic_HC_SR04::distanceInMicroseconds()
-{
-    digitalWrite(pin_trigger,LOW);
-    delayMicroseconds(2);
-    digitalWrite(pin_trigger,HIGH);
+/* ------------------------------ SIMPLE APIs ------------------------------ */
+
+/* Method - It returns the distance in microseconds */
+int UltrasonicSensor::distanceInMicroseconds() {
+    digitalWrite(this->pinTrigger, HIGH);
     delayMicroseconds(10);
-    digitalWrite(pin_trigger,LOW);
+    digitalWrite(this->pinTrigger, LOW);
 
-    return (pulseIn(this->pin_echo, HIGH));
+    return (pulseIn(this->pinEcho, HIGH) / 2);
 }
 
-double UltraSonic_HC_SR04::distancia_cm()
-{
-    double distancia_cm=this->distanceInMicroseconds()*0.034/2;
-    return distancia_cm;
+/* Method - It returns the external temperature */
+float UltrasonicSensor::getTemperature() {
+    return this->temperature;
+}
+
+/* Method - It sets the external temperature */
+void UltrasonicSensor::setTemperature(float temperature) {
+    this->temperature = temperature;
+}
+
+/* ------------------------------ OTHER FUNCTIONS ------------------------------ */
+
+/* Method - It returns the distance in centimeters */
+int UltrasonicSensor::distanceInCentimeters() {
+    double reciprocalVelocity = 10000 / (331.45 + (0.62 * this->temperature));  //  reciprocalVelocity = 1 / velocity
+    int duration = this->distanceInMicroseconds();
+    int centimeters = (duration / reciprocalVelocity);                          //  space   = duration * velocity =
+                                                                                //          = duration / reciprocalVelocity
+
+    return centimeters;
+}
+
+/* Method - It returns the distance in millimeters */
+int UltrasonicSensor::distanceInMillimeters() {
+    double reciprocalVelocity = 1000 / (331.45 + (0.62 * this->temperature));  //  reciprocalVelocity = 1 / velocity
+    int duration = this->distanceInMicroseconds();
+    int millimeters = (duration / reciprocalVelocity);                          //  space   = duration * velocity =
+                                                                                //          = duration / reciprocalVelocity
+
+    return millimeters;
+}
+
+/* Method - It calibrates the Ultrasonic sensor */
+void UltrasonicSensor::calibration(int realDistance) {
+    double velocity = (((double) realDistance) / this->distanceInMicroseconds()) * 10000;
+    this->setTemperature((velocity - 331.45) / 0.62);
+}
+
+/* Method - It returns the UltrasonicSensor informations */
+String UltrasonicSensor::toString() {
+    String ret = String("Ultrasonic: {");
+    ret.concat("pinTrigger=");
+    ret.concat(this->pinTrigger);
+    ret.concat(", pinEcho=");
+    ret.concat(this->pinEcho);
+    ret.concat(", temperature=");
+    ret.concat(this->temperature);
+    ret.concat("}");
+    
+    return ret;
 }
